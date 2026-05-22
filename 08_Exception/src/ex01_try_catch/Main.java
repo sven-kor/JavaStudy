b7c33a9ac7b46268f8c53bed05cf05bf1673872a
sven-kor <164502736+sven-kor@users.noreply.github.com>
2026-05-22 15:09:57 +0900

    try_catch

---
+++ 08_Exception/src/ex01_try_catch/Main.java
@@ -0,0 +1,32 @@
+package ex01_try_catch;
+
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int a, b;
+
+    while (true) {
+
+      try {
+        System.out.println("첫 번째 숫자 입력");
+        a = sc.nextInt();
+        System.out.println("두 번째 숫자 입력");
+        b = sc.nextInt();
+        // 0으로 나누면 예외발생! -> 처리 안하면 웹 먹통 되는 것!
+        System.out.println("몫:" + a / b + "나머지:" + a % b);
+      } catch (ArithmeticException e) {
+        // err문 출력시
+        // System.err.println("에러 발생 : " + e.getMessage());
+        e.printStackTrace();
+      } catch (InputMismatchException e) {
+        System.out.println("정수만 입력할 수 있습니다.");
+      } finally {
+        System.out.println("다시 입력하세요.");
+      }
+    }
+  }
+
+}
